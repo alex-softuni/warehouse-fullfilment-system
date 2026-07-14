b67bbc2c7edad80f04d5934d7d61059717973051
alex-softuni <notorious.atanasow@gmail.com>
2026-07-14 08:22:51 +0300

    INITIAL COMMIT

---
+++ src/main/java/com/ft/warehousefullfilmentsystem/WarehouseFullfilmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ft.warehousefullfilmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseFullfilmentSystemApplication {
+
+     static void main(String[] args) {
+        SpringApplication.run(WarehouseFullfilmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ft/warehousefullfilmentsystem/WarehouseFullfilmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ft.warehousefullfilmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseFullfilmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
