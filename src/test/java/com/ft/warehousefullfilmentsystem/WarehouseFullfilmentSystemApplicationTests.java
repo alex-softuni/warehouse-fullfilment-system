c5108bbb233494c85ab2c894830d11bacf14524d
alex-softuni <notorious.atanasow@gmail.com>
2026-07-28 14:31:29 +0300

    test: add PostgreSQL Testcontainers integration

--- src/test/java/com/ft/warehousefullfilmentsystem/WarehouseFullfilmentSystemApplicationTests.java
+++ src/test/java/com/ft/warehousefullfilmentsystem/WarehouseFullfilmentSystemApplicationTests.java
@@ -2,12 +2,21 @@ package com.ft.warehousefullfilmentsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 @SpringBootTest
+@Testcontainers
 class WarehouseFullfilmentSystemApplicationTests {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer postgres =
+            new PostgreSQLContainer("postgres:17-alpine");
 
     @Test
     void contextLoads() {
     }
-
 }
